fd050e89032e6fdb1d25bdd04209e6de0a7e7eae
Vladyslav Rudik <salon200390@gmail.com>
2025-12-20 23:02:35 +0200

    Update api.js

--- backend/routes/api.js
+++ backend/routes/api.js
@@ -3,8 +3,12 @@ const router = express.Router();
 
 const healthController = require("../controllers/healthController");
 const demoController = require("../controllers/demoController");
+const mangaController = require("../controllers/mangaController");
 
 router.get("/health", healthController.healthCheck);
 router.get("/error-demo", demoController.throwDemoError);
+
+// Demo DB endpoint
+router.get("/mangas", mangaController.listMangas);
 
 module.exports = router;
