a2242348cd13db790cfb6827210105d2637c5346
Vladyslav Rudik <salon200390@gmail.com>
2025-12-20 04:23:43 +0200

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -5,9 +5,17 @@ const PORT = 3000;
 const indexRouter = require("./routes/index");
 const apiRouter = require("./routes/api");
 
+const requestLogger = require("./middlewares/requestLogger");
+const errorHandler = require("./middlewares/errorHandler");
+
 app.use(express.json());
+app.use(requestLogger);
+
 app.use("/", indexRouter);
 app.use("/api", apiRouter);
+
+// Global error handler (always last)
+app.use(errorHandler);
 
 app.listen(PORT, () => {
     console.log(`Demo server running on http://localhost:${PORT}`);
