aecccf723f666f74a7351b6a7c51178b104f9da7
Vladyslav Rudik <salon200390@gmail.com>
2025-12-23 20:04:34 +0200

    Update index.js

--- backend/routes/v1/index.js
+++ backend/routes/v1/index.js
@@ -5,6 +5,12 @@ const healthController = require("../../controllers/healthController");
 const demoController = require("../../controllers/demoController");
 const mangaController = require("../../controllers/mangaController");
 
+const userController = require("../../controllers/userController");
+
+router.get("/users", userController.listUsers);
+router.get("/users/:id", userController.getUser);
+
+
 router.get("/health", healthController.healthCheck);
 router.get("/error-demo", demoController.throwDemoError);
 
