74417583ac4f9ec744f8364ea492d0818a56d874
Vladyslav Rudik <salon200390@gmail.com>
2025-12-23 20:00:59 +0200

    Create index.js

---
+++ backend/routes/v1/index.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const router = express.Router();
+
+const healthController = require("../../controllers/healthController");
+const demoController = require("../../controllers/demoController");
+const mangaController = require("../../controllers/mangaController");
+
+router.get("/health", healthController.healthCheck);
+router.get("/error-demo", demoController.throwDemoError);
+
+router.get("/mangas", mangaController.listMangas);
+router.get("/mangas/:id", mangaController.getManga);
+
+module.exports = router;
