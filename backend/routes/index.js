376288412b0f027728fd6c1744cf0e6e252d6231
Vladyslav Rudik <salon200390@gmail.com>
2025-12-25 18:01:53 +0200

    Update index.js

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,5 +1,14 @@
 const express = require("express");
 const router = express.Router();
+const requireAuth = require("../../middlewares/authMiddleware");
+
+router.get("/me", requireAuth, (req, res) => {
+    res.json({
+        data: {
+            user: req.user
+        }
+    });
+});
 
 router.get("/", (req, res) => {
     res.send("Welcome to the SoftManga Demo Site Backend");
